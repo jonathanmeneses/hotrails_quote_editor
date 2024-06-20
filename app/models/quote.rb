ce2c759b75e2f3e4b94d1502155df8948fa0034d
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-06-19 23:04:45 -0500

    create lineitemdate controller up to 'Updating line item dates without Turbo' section

--- app/models/quote.rb
+++
@@ -1,9 +0,0 @@
-class Quote < ApplicationRecord
-  validates :name, presence: true
-
-  scope :ordered, -> { order(id: :desc) }
-
-  belongs_to :company
-
-  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
-end
