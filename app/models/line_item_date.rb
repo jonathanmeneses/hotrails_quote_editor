7e47375de205be71c827b8fb29fa196aa0d3efa8
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-06-24 22:51:45 -0500

    create fixtures

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -5,6 +5,8 @@ class LineItemDate < ApplicationRecord
 
   scope :ordered, -> {order(date: :asc)}
 
+  has_many :line_items, dependent: :destroy
+
   def previous_date
     quote.line_item_dates.ordered.where("date < ? ", date).last
   end
